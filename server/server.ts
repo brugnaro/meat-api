778d6640c585533edb4fe778cf6a12778b0ae7bc
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-03-03 09:49:40 -0300

    Connect to mongo

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import * as restify from 'restify'
+import * as mongoose from 'mongoose'
 import { environment } from '../common/environment'
 import { Router } from '../common/router'
 
@@ -6,7 +7,14 @@ export class Server {
 
   application: restify.Server
 
-  initRoutes(routers:Router[]): Promise<any> {
+  initializeDb(): mongoose.MongooseThenable {
+    (<any>mongoose).Promise = global.Promise
+    return mongoose.connect(environment.db.url, {
+      useMongoClient: true
+    })
+  }
+
+  initRoutes(routers: Router[]): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
 
@@ -18,7 +26,7 @@ export class Server {
         this.application.use(restify.plugins.queryParser())
 
         //routes
-        for(let router of routers){
+        for (let router of routers) {
           router.applyRoutes(this.application)
         }
 
@@ -33,7 +41,8 @@ export class Server {
   }
 
   bootstrap(routers: Router[] = []): Promise<Server> {
-    return this.initRoutes(routers).then(() => this)
+    return this.initializeDb().then(() =>
+      this.initRoutes(routers).then(() => this))
   }
 
 }
